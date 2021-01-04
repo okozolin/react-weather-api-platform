6425e474c7cd5fed48311c1ea2663b1e77b4c981
Orit Kozolin <orit@nachum.co.il>
2021-01-04 09:25:31 +0200

    title - running clock

---
+++ client-weather-alerts/src/components/Header.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import { Typography, Box, Grid, Button } from "@material-ui/core";
+import Moment from "react-moment";
+
+Moment.globalFormat = "D MMM YYYY";
+
+export default function Header() {
+    const [date, setDate] = useState(new Date())
+
+useEffect(() => {
+    const intervalId = setInterval(() => setDate(new Date()), 1000)
+    return () => {
+        clearInterval(intervalId)
+    }
+},[])
+
+    return (
+        <Box 
+            display="flex" 
+            alignItems="center" 
+            justifyContent="center" 
+            fontFamily='Roboto Condensed' 
+            fontSize={56} p={4} 
+            bgcolor="#06D6A0" 
+            color="#073B4C">
+            <Moment format="DD/MM/YYYY HH:mm">{date}</Moment>
+        </Box>
+    )
+}
