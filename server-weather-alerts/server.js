b681e650e44e80b3cd3efba1820007b017b2d11a
Orit Kozolin <orit@nachum.co.il>
2021-01-19 18:34:22 +0200

    changes for dev vs prod

--- server-weather-alerts/server.js
+++ server-weather-alerts/server.js
@@ -2,28 +2,28 @@ const express = require('express');
 const path= require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
-const cors= require("cors")
 const initRoutes= require("./routes")
 require("dotenv").config();
 const app = express();
 
-const corsOptions = {
-    origin: process.env.CLIENT_PORT
-  };
-  
-app.use(cors(corsOptions));
+const NODE_ENV = process.env.NODE_ENV || 'development';
+if (NODE_ENV === 'development') {
+  console.log("--> DEV mode")
+  const cors= require("cors")
+  const corsOptions = {
+      origin: process.env.CLIENT_PORT
+    }; 
+  app.use(cors(corsOptions));
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+if (NODE_ENV === 'production') {
+  console.log("--> running from 'public'")
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
-
-let port = process.env.PORT || 3002;
-app.listen(port, () => {
-  console.log(`Running at localhost:${port}`);
-});
 
 module.exports = app;
